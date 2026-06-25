82ae6ce383222fbb02a9ff15c09b3a42df3eb1c3
moidoubi <moidoubi@student.42.fr>
2026-06-25 10:47:19 +0200

    fix fd

--- src/executor/exec_pipe.c
+++ src/executor/exec_pipe.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_pipe.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aakli <aakli@student.42.fr>                +#+  +:+       +#+        */
+/*   By: moidoubi <moidoubi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/07 00:35:45 by moidoubi          #+#    #+#             */
-/*   Updated: 2026/06/23 19:07:32 by aakli            ###   ########.fr       */
+/*   Updated: 2026/06/25 09:25:47 by moidoubi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,24 @@
 
 void	fork_left(t_node *node, t_shell *shell, int *pipefd)
 {
+	int status;
+	
 	close(pipefd[0]);
 	dup2(pipefd[1], 1);
 	close(pipefd[1]);
-	exit(execute_ast(node->left, shell));
+	status = execute_ast(node->left, shell);
+	close(1);
+	exit(status);
 }
 
 void	fork_right(t_node *node, t_shell *shell, int *pipefd)
 {
+	int status;
+
 	close(pipefd[1]);
 	dup2(pipefd[0], 0);
 	close(pipefd[0]);
-	exit(execute_ast(node->right, shell));
+	status = execute_ast(node->right, shell);
+	close(0);
+	exit(status);
 }
